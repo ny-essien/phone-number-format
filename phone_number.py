8e1e174fe999716f49d5824dc7da619496c9b81a
Nsikan Essien <nsikan4001@gmail.com>
2023-02-15 17:39:39 +0100

    phone number

---
+++ phone_number.py
@@ -0,0 +1,39 @@
+#write a function that takes in a string
+#containing a phone number and presents
+#it in the format '1234 - 5678 - 901'
+
+def phone_number(number : str = "") -> str:
+
+    count = 0
+    stack = []
+    result = ""
+    code = '234 - '
+
+
+    for i in number:
+
+        count += 1
+        stack.append(i)
+
+        if count == 4:
+            stack.append(' ')
+            stack.append('-')
+            stack.append(' ')
+
+        if count == 8:
+            stack.append(' ')
+            stack.append('-')
+            stack.append(" ")
+
+    result = result.join(stack)
+    result = code + result
+
+    for x in range(len(stack)):
+        stack.pop()
+
+    return result
+
+if __name__=="__main__":
+    print(phone_number("08064725214"))
+
+    
